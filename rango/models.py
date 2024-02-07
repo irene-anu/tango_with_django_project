5ab7678a6642967f0da67e3ef171d73c7f735773
Irene Anu <2772146I@student.gla.ac.uk>
2024-02-07 12:22:20 +0000

    Chapter 5

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length = 128, unique = True)  #This key can be used as primary key as it is unique throughout the database
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
+
+# You can also specify additional attributes for each field, such as stating a default
+# value with the syntax default='value', and whether the value for a field can be blank
+# (or NULL¹¹) (null=True) or not (null=False)
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)  # this is a foreign key, help create one to many relationship with Category 
+    # Cascade instructs Django to delete the pages associated with category when category is deleted. 
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.title
+    
